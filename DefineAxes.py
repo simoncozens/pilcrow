81b00321408177cf1f90d8daf2d23239c8087391
Simon Cozens <simon@simon-cozens.org>
2020-11-28 20:42:17 +0000

    Lots of validation

--- DefineAxes.py
+++ DefineAxes.py
@@ -2,8 +2,10 @@ from PyQt5.QtWidgets import *
 from MyWizardPage import MyWizardPage
 from MapEditor import MapEditor
 from fontTools.designspaceLib import AxisDescriptor
-from PyQt5.QtCore import Qt, pyqtSlot, QPoint
+from PyQt5.QtCore import Qt, pyqtSlot, QRegularExpression
 from PyQt5 import QtGui
+from PyQt5.QtGui import QValidator
+import re
 
 tagToolTip =  """<qt>Four letter tag for this axis. Some might be registered at the OpenType specification. Privately-defined axis tags must begin with an uppercase letter and use only uppercase letters or digits.</qt>"""
 minToolTip = "<qt>The minimum value for this axis in user space.</qt>"
@@ -11,6 +13,64 @@ maxToolTip = "<qt>The maximum value for this axis in user space.</qt>"
 defaultToolTip = """<qt>The default value for this axis, i.e. when a new location is created, this is the value this axis will get in user space.</qt>"""
 hiddenToolTip = """While most axes are intended to be exposed to the user through a \"slider\" or other interface, some axes (such as <i>opsz</i> or parametric axes) are normally hidden. If this checkbox is set, the axis will be exposed to the user."""
 mapToolTip = """<qt>Edits the mapping between userspace and designspace coordinates (avar table).</qt>"""
+registeredAxes = {
+  "italic": {
+    "tag": "ital",
+    "min": 0,
+    "minFrozen": True,
+    "max": 1,
+    "maxFrozen": True,
+    "default": 0,
+    "defaultFrozen": True,
+    "hidden": False
+  },
+  "optical size": {
+    "tag": "opsz",
+    "min": 1,
+    "max": 18,
+    "default": 9,
+    "defaultFrozen": False,
+    "hidden": True
+  },
+  "slant": {
+    "tag": "slnt",
+    "min": -90,
+    "minFrozen": True,
+    "max": 90,
+    "maxFrozen": True,
+    "default": 0,
+    "defaultFrozen": True,
+    "hidden": False
+  },
+  "width": {
+    "tag": "wdth",
+    "min": 50,
+    "max": 200,
+    "default": 100,
+    "defaultFrozen": True,
+    "hidden": False
+  },
+  "weight": {
+    "tag": "wght",
+    "min": 1,
+    "minFrozen": False,
+    "max": 1000,
+    "maxFrozen": False,
+    "default": 400,
+    "defaultFrozen": True,
+    "hidden": False
+  }
+}
+tag_re = "^("+ "|".join([x["tag"] for x in registeredAxes.values()])+ r"|[A-Z]{4})$"
+
+
+class TagValidator(QValidator):
+    def validate(self, s, pos):
+        if re.match(tag_re, s):
+            return (QValidator.Acceptable, s, pos)
+        if s and not re.match(r"^([a-z]+|[A-Z]+)$",s):
+            return (QValidator.Invalid, s, pos)
+        return (QValidator.Intermediate, s, pos)
 
 from enum import IntEnum
 class Col(IntEnum):
@@ -24,54 +84,6 @@ class Col(IntEnum):
   REMOVE = 7
 
 class DefineAxes(MyWizardPage):
-  registeredAxes = {
-    "italic": {
-      "tag": "ital",
-      "min": 0,
-      "minFrozen": True,
-      "max": 1,
-      "maxFrozen": True,
-      "default": 0,
-      "defaultFrozen": True,
-      "hidden": False
-    },
-    "optical size": {
-      "tag": "opsz",
-      "min": 1,
-      "max": 18,
-      "default": 9,
-      "defaultFrozen": False,
-      "hidden": True
-    },
-    "slant": {
-      "tag": "slnt",
-      "min": -90,
-      "minFrozen": True,
-      "max": 90,
-      "maxFrozen": True,
-      "default": 0,
-      "defaultFrozen": True,
-      "hidden": False
-    },
-    "width": {
-      "tag": "wdth",
-      "min": 50,
-      "max": 200,
-      "default": 100,
-      "defaultFrozen": True,
-      "hidden": False
-    },
-    "weight": {
-      "tag": "wght",
-      "min": 1,
-      "minFrozen": False,
-      "max": 1000,
-      "maxFrozen": False,
-      "default": 400,
-      "defaultFrozen": True,
-      "hidden": False
-    }
-  }
 
   def __init__(self, parent=None):
     super(QWizardPage, self).__init__(parent)
@@ -93,11 +105,11 @@ class DefineAxes(MyWizardPage):
     if not self.designspace.axes:
       self.designspace.axes = [AxisDescriptor(
         name="weight",
-        tag = self.registeredAxes["weight"]["tag"],
-        minimum = self.registeredAxes["weight"]["min"],
-        maximum = self.registeredAxes["weight"]["max"],
-        default = self.registeredAxes["weight"]["default"],
-        hidden = self.registeredAxes["weight"]["hidden"],
+        tag = registeredAxes["weight"]["tag"],
+        minimum = registeredAxes["weight"]["min"],
+        maximum = registeredAxes["weight"]["max"],
+        default = registeredAxes["weight"]["default"],
+        hidden = registeredAxes["weight"]["hidden"],
         )]
     self.setupAxes()
 
@@ -120,21 +132,21 @@ class DefineAxes(MyWizardPage):
   def setDefaultValues(self, ix):
     axis = self.designspace.axes[ix]
     name = axis.name.lower().strip()
-    if name in self.registeredAxes:
-      axisDefaults = self.registeredAxes[name]
+    if name in registeredAxes.keys():
+      axisDefaults = registeredAxes[name]
       axis.tag = axisDefaults["tag"]
       axis.minimum = axisDefaults["min"]
       axis.maximum = axisDefaults["max"]
       axis.default = axisDefaults["default"]
       axis.hidden = axisDefaults["hidden"]
-      self.axesLayout.itemAtPosition(ix+1,Col.TAG).widget().setText(axis.tag)
-      self.axesLayout.itemAtPosition(ix+1,Col.MIN).widget().setValue(axis.minimum)
-      self.axesLayout.itemAtPosition(ix+1,Col.MAX).widget().setValue(axis.maximum)
-      self.axesLayout.itemAtPosition(ix+1,Col.DEFAULT).widget().setValue(axis.default)
-      self.axesLayout.itemAtPosition(ix+1,Col.VISIBLE).widget().setChecked(not axis.hidden)
+      self.getWidget(ix, Col.TAG).setText(axis.tag)
+      self.getWidget(ix, Col.MIN).setValue(axis.minimum)
+      self.getWidget(ix, Col.MAX).setValue(axis.maximum)
+      self.getWidget(ix, Col.DEFAULT).setValue(axis.default)
+      self.getWidget(ix, Col.VISIBLE).setChecked(not axis.hidden)
     elif not axis.tag or len(axis.tag) < 4:
       axis.tag = self.suggestTag(axis)
-      self.axesLayout.itemAtPosition(ix+1,Col.TAG).widget().setText(axis.tag)
+      self.getWidget(ix, Col.TAG).setText(axis.tag)
 
   def suggestTag(self, axis):
     name = axis.name.upper().replace(" ", '')
@@ -154,9 +166,13 @@ class DefineAxes(MyWizardPage):
   def setTag(self):
     print("Tag set")
     axis = self.designspace.axes[self.sender().ix]
+    state = self.sender().validator().validate(self.sender().text(), 0)[0]
     axis.tag = self.sender().text()
-    self.completeChanged.emit()
     self.parent.dirty = True
+    self.completeChanged.emit()
+
+  def getWidget(self, ix, col):
+    return self.axesLayout.itemAtPosition(ix+1,col).widget()
 
   @pyqtSlot()
   def setMin(self):
@@ -164,8 +180,8 @@ class DefineAxes(MyWizardPage):
     ix = self.sender().ix
     axis = self.designspace.axes[ix]
     axis.minimum = float(self.sender().value())
-    self.axesLayout.itemAtPosition(ix+1,Col.MAX).widget().setMinimum(axis.minimum)
-    self.axesLayout.itemAtPosition(ix+1,Col.DEFAULT).widget().setMinimum(axis.minimum)
+    self.getWidget(ix, Col.MAX).setMinimum(axis.minimum)
+    self.getWidget(ix, Col.DEFAULT).setMinimum(axis.minimum)
     print(axis.serialize())
     self.completeChanged.emit()
     self.parent.dirty = True
@@ -175,9 +191,10 @@ class DefineAxes(MyWizardPage):
     print("Max set")
     ix = self.sender().ix
     axis = self.designspace.axes[ix]
+    print(axis.serialize())
     axis.maximum = float(self.sender().value())
-    self.axesLayout.itemAtPosition(ix+1,Col.MIN).widget().setMinimum(axis.minimum)
-    self.axesLayout.itemAtPosition(ix+1,Col.DEFAULT).widget().setMinimum(axis.minimum)
+    self.getWidget(ix, Col.MIN).setMaximum(axis.maximum)
+    self.getWidget(ix, Col.DEFAULT).setMaximum(axis.maximum)
     print(axis.serialize())
     self.completeChanged.emit()
     self.parent.dirty = True
@@ -233,6 +250,7 @@ class DefineAxes(MyWizardPage):
       tag = QLineEdit()
       tag.ix = ix
       tag.setPlaceholderText("Tag")
+      tag.setValidator(TagValidator())
       tag.setToolTip(tagToolTip)
       # validator XXX
       tag.setMaxLength(4)
@@ -257,8 +275,10 @@ class DefineAxes(MyWizardPage):
       default = QSpinBox()
       default.ix = ix
       default.setToolTip(defaultToolTip)
-      default.setMinimum(ax.minimum)
-      default.setMaximum(ax.maximum)
+      if ax.minimum is not None:
+        default.setMinimum(ax.minimum)
+      if ax.maximum is not None:
+        default.setMaximum(ax.maximum)
       if ax.default is not None:
         default.setValue(ax.default)
       default.valueChanged.connect(self.setDefault)
@@ -267,10 +287,8 @@ class DefineAxes(MyWizardPage):
       maxBox = QSpinBox()
       maxBox.ix = ix
       maxBox.setToolTip(maxToolTip)
-      maxBox.setMinimum(ax.minimum)
-      maxBox.setMaximum(ax.maximum)
-      maxBox.setSizePolicy(QSizePolicy.Fixed, QSizePolicy.Fixed)
-      maxBox.setMaximumWidth(85)
+      maxBox.setMinimum(0)
+      maxBox.setMaximum(1000)
       if ax.maximum is not None:
         maxBox.setValue(ax.maximum)
       maxBox.valueChanged.connect(self.setMax)
@@ -279,8 +297,6 @@ class DefineAxes(MyWizardPage):
       visibleInUI = QCheckBox("")
       visibleInUI.ix = ix
       visibleInUI.setToolTip(hiddenToolTip)
-      visibleInUI.setMaximumWidth(85)
-      visibleInUI.setSizePolicy(QSizePolicy.Fixed, QSizePolicy.Fixed)
 
       if not ax.hidden:
         visibleInUI.setChecked(True)
@@ -303,14 +319,45 @@ class DefineAxes(MyWizardPage):
 
   def isComplete(self):
     if len(self.designspace.axes) < 1: return False
-    for axis in self.designspace.axes:
-      if not axis.name: return False
-      if not axis.tag: return False
-      if not axis.minimum: return False
-      if not axis.maximum: return False
-      if not axis.default: return False
-      if axis.minimum > axis.maximum: return False
-      if not (axis.minimum <= axis.default <= axis.maximum): return False
-    return True
+    seenAxis = {}
+    firstError = None
+    # Validate whole form
+    for ix,axis in enumerate(self.designspace.axes):
 
+      nameW = self.getWidget(ix,Col.NAME)
+      if not axis.name:
+        nameW.setStyleSheet("border-color: #B00020")
+        firstError = firstError or nameW
+      else:
+        nameW.setStyleSheet("")
+
+      axisW = self.getWidget(ix,Col.TAG)
+      if not axis.tag or not re.match(tag_re, axis.tag) or axis.tag in seenAxis:
+        axisW.setStyleSheet("border-color: #B00020")
+        firstError = firstError or axisW
+      else:
+        axisW.setStyleSheet("")
+      seenAxis[axis.tag] = True
+
+      minW = self.getWidget(ix,Col.MIN)
+      if not axis.minimum or axis.minimum > axis.maximum:
+        minW.setStyleSheet("border-color: #B00020")
+        firstError = firstError or minW
+      else:
+        minW.setStyleSheet("")
+
+      maxW = self.getWidget(ix,Col.MAX)
+      if not axis.maximum:
+        maxW.setStyleSheet("border-color: #B00020")
+        firstError = firstError or maxW
+      else:
+        maxW.setStyleSheet("")
+
+      defaultW = self.getWidget(ix,Col.DEFAULT)
+      if not axis.default or not (axis.minimum <= axis.default <= axis.maximum):
+        defaultW.setStyleSheet("border-color: #B00020")
+        firstError = firstError or defaultW
+      else:
+        minW.setStyleSheet("")
+    return not firstError
 
